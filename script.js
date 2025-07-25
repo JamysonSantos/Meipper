f09ed954c7a63f0bed2d310f6e509b4684457fdd
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2025-07-25 18:01:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Global variables
 const COLORS = ['#2196f3', '#f44336', '#4caf50', '#ff9800', '#9c27b0', '#3f51b5', '#009688', '#795548'];
 const EXTENDED_COLORS = ['#607d8b', '#e91e63', '#cddc39', '#00bcd4', '#ffc107', '#8bc34a', '#ff5722', '#673ab7'];
 
