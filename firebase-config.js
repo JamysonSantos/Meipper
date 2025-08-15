80c7b7d16c013dc16ede9e1e99ad42f78ad5334e
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2025-08-15 18:46:38 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,7 +10,12 @@ import {
 
 // Configuração do Firebase
 const firebaseConfig = {
-  // 🔹 Cole aqui suas credenciais do Firebase
+  apiKey: "AIzaSyC-V5suvNo46FndhUbqMiIomiuVma3li-w",
+  authDomain: "meipper-74267.firebaseapp.com",
+  projectId: "meipper-74267",
+  storageBucket: "meipper-74267.firebasestorage.app",
+  messagingSenderId: "619482964791",
+  appId: "1:619482964791:web:2937f0ecead659e54d30b2"
 };
 
 // Inicializar app primeiro
